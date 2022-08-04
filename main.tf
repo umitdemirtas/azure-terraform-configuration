0db7d5519168e4912e6c4722aed5e81e05b711d0
Umit <47488092+umitdemirtas@users.noreply.github.com>
2022-08-05 02:10:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,23 +12,3 @@ provider "azurerm" {
     
   }
 }
-module test1 {
-    source              = "./Module"
-
-    # resource group
-    location            = "West Europe"
-
-    # vm
-    virtual_machine_name = "test1"
-    admin_username = "test1"
-} # endmodule "test1"
-module test12 {
-    source              = "./Module"
-
-    # resource group
-    location            = "France Central"
-
-    # vm
-    virtual_machine_name = "test12"
-    admin_username = "test12"
-} # endmodule "test12"
