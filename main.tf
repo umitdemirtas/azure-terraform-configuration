ef7121edf6a47b1e87a989be6c032ac9d52c577b
umitdemirtas <umitdemirtas>
2022-08-05 10:50:09 +0300

    test7 add configuration

--- main.tf
+++ main.tf
@@ -12,3 +12,15 @@ provider "azurerm" {
     
   }
 }
+module test7 {
+    source              = "./Module"
+
+    # resource group
+    location            = "West Europe"
+
+    # vm
+    virtual_machine_name = "test7"
+    admin_username = "test7"
+
+    virtual_machine_size = "Standard_A4_v2"
+} # endmodule "test7"
