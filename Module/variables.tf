5ea9a99c9b113e22e028dd7362e2d651fdbdde48
“Umit” <extern.demirtas_umit@allianz.de>
2022-08-05 03:16:24 +0300

    added some comments

--- Module/variables.tf
+++ Module/variables.tf
@@ -10,7 +10,7 @@ variable "virtual_machine_name" {
 
 variable "virtual_machine_size" {
   description = "The size of the virtual machine."
-  default     = "Standard_A2_v2"
+  default     = "Standard_A2_v2" ## Standard_A2_v2, Standard_A1_v2, Standard_A4_v2
 }
 
 variable "admin_username" {
